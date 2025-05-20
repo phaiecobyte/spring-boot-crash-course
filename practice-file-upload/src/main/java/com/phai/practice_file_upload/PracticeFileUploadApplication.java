9343c4f39075a153a32cf8391ff080164eae2589
phaiecobyte <brave6823@gmail.com>
2025-05-20 21:08:32 +0700

    practice file upload in spring boot

---
+++ practice-file-upload/src/main/java/com/phai/practice_file_upload/PracticeFileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.phai.practice_file_upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeFileUploadApplication.class, args);
+	}
+
+}
---
+++ practice-file-upload/src/test/java/com/phai/practice_file_upload/PracticeFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phai.practice_file_upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeFileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
