03228b12687b17b6b4ed2aab672f05b8e4f4cdfb
phaiecobyte <phaieboyte@gmail.com>
2025-10-01 11:45:34 +0700

    project setup

---
+++ sso-spring-security-oauth2/src/main/java/com/phaiecobyte/ssospringsecurityoauth2/SsoSpringSecurityOauth2Application.java
@@ -0,0 +1,13 @@
+package com.phaiecobyte.ssospringsecurityoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoSpringSecurityOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoSpringSecurityOauth2Application.class, args);
+    }
+
+}
---
+++ sso-spring-security-oauth2/src/test/java/com/phaiecobyte/ssospringsecurityoauth2/SsoSpringSecurityOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phaiecobyte.ssospringsecurityoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoSpringSecurityOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
