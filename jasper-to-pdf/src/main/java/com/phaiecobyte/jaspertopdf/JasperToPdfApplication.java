1e0623849ac29921eec05a8f1ef3631e231178c6
phaiecobyte <phaieboyte@gmail.com>
2025-10-06 08:35:55 +0700

    setup module jasper-to-pdf

---
+++ jasper-to-pdf/src/main/java/com/phaiecobyte/jaspertopdf/JasperToPdfApplication.java
@@ -0,0 +1,13 @@
+package com.phaiecobyte.jaspertopdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperToPdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JasperToPdfApplication.class, args);
+    }
+
+}
---
+++ jasper-to-pdf/src/test/java/com/phaiecobyte/jaspertopdf/JasperToPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phaiecobyte.jaspertopdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperToPdfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
